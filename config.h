8f40b93267981a47c6b3c206f1d3b699585d342d
dmytro <dmytro@perkov.ca>
2020-05-12 13:21:08 -0400

    merged extrabar and alpha

--- config.h
+++ config.h
@@ -10,6 +10,7 @@ static const unsigned int gappov    = 10;       /*  vert outer gap between windo
 static const int smartgaps          = 0;        /*  1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char statussep         = ';';      /*  separator between status bars */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
