839b5e460e6cd902597a73059692ba5662089a73
dmytro <dmytro@perkov.ca>
2020-05-15 01:17:12 -0400

    pywal integration

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /*  horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /*  vert inner gap between windows */
@@ -47,7 +47,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
