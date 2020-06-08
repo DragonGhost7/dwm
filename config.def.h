9f75781f0e91b8cf192715221d2179a1bececbd7
DragonGhost7 <darkiridiumghost@gmail.com>
2020-06-08 17:44:56 -0400

    swallow patch applied

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@ static const unsigned int gappiv    = 10;       /*  vert inner gap between windo
 static const unsigned int gappoh    = 10;       /*  horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /*  vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /*  1 means no outer gap when there is only one window */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -45,11 +46,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Spotify",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{   NULL   ,  NULL,    "neomutt",     1 << 3,       0,           -1 },
-	{"Brave-browser", NULL,   NULL,       1 << 1,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,          0,             0,       -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 8,       0,          0,             0,        -1 },
+	{   NULL   ,  NULL,    "neomutt",     1 << 3,       0,          0,             0,         -1 },
+	{"Brave-browser", NULL,   NULL,       1 << 1,       0,          0,             -1,         -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
+
 };
 
 /* layout(s) */
