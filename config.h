7ae5bec40723c0c90264f515420c484ac7958cc6
DragonGhost7 <darkiridiumghost@gmail.com>
2022-01-26 18:44:10 -0500

    added emoji support

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
 static const char statussep         = ';';      /*  separator between status bars */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10","JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
