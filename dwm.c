74e7e51a67ae1b50ec237074b4d0a8f537652d64
DragonGhost7 <darkiridiumghost@gmail.com>
2022-01-26 18:44:10 -0500

    applied centertitle patch, changed it so it shows it in the middle of the title bar and not of the screen, because ocd

--- dwm.c
+++ dwm.c
@@ -953,8 +953,11 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
+			int mid = (m->ww - x - tw - TEXTW(m->sel->name)) / 2;
+			/* make sure name will not overlap on tags even when it is very long */
+			mid = mid >= lrpad / 2 ? mid : lrpad / 2;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
